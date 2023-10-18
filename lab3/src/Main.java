3aa1597b62e851211dd3c36666b287d7455d1fd7
Stancu Alex <stancualexdaniel7@gmail.com>
2023-10-18 16:18:04 +0300

    add lab3

---
+++ lab3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        if (0.1 + 0.1 + 0.1 == 0.3) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
